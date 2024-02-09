b908c8792f125a30227096aeddac0c0cc6838f43
morgan-b-kim <morgan-b-kim@gmail.com>
2024-02-10 06:18:41 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/do_something', methods=['GET', 'POST'])
+def do_something():
+    # 여기에 특정 기능을 수행하는 코드를 작성합니다.
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)

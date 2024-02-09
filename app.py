66ce6f777638485866998b76dbd076c56b80b5eb
morgan-b-kim <morgan-b-kim@gmail.com>
2024-02-10 06:21:21 +0900

    first

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route('/do_something', methods=['GET', 'POST'])
 def do_something():
     # 여기에 특정 기능을 수행하는 코드를 작성합니다.
+    print('do_someting')
     return 'Hello, World!'
 
 if __name__ == '__main__':

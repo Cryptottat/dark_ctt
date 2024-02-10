c5e6afb7a524b5b58a3e31b294ca2edf24542ed3
morgan-b-kim <morgan-b-kim@gmail.com>
2024-02-10 15:44:58 +0900

    first

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+with open('api_key.txt', 'r') as file:
+    api_key = file.read().strip()
+with open('secret_key.txt', 'r') as file:
+    secret_key = file.read().strip()
+
+# exchange = ccxt.bybit({
+#     'apiKey': api_key,
+#     'secret': secret_key,
+# })
+
 @app.route('/do_something', methods=['GET', 'POST'])
 def do_something():
     # 여기에 특정 기능을 수행하는 코드를 작성합니다.
     print('do_someting')
+    # exchange.cancel_all()
     return 'Hello, World!'
 
 if __name__ == '__main__':

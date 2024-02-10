33d26fbcee61ae40db81d461db3c30bdfdd1b358
morgan-b-kim <morgan-b-kim@gmail.com>
2024-02-10 16:09:59 +0900

    first

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 with open('api_key.txt', 'r') as file:
@@ -13,10 +13,20 @@ with open('secret_key.txt', 'r') as file:
 
 @app.route('/do_something', methods=['GET', 'POST'])
 def do_something():
-    # 여기에 특정 기능을 수행하는 코드를 작성합니다.
-    print('do_someting')
-    # exchange.cancel_all()
-    return 'Hello, World!'
+    if not request.method == 'POST':
+        return
+    # POST 요청에서 JSON 데이터 인자를 받습니다.
+    data = request.json  # JSON 데이터 전체를 파이썬 딕셔너리로 받습니다.
+    symbol = data.get('symbol')
+    price = data.get('price')
+    qty = data.get('qty')
+    print(f'Received: Symbol={symbol}, Price={price}, Quantity={qty}')
+    response = {
+        'status': 'success',
+        'message': f'Received symbol: {symbol}, price: {price}, and quantity: {qty}'
+    }
+    # 여기에 나머지 코드를 작성합니다.
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
